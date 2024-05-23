f7f2c7b07bf5c71df0b15ab38a6988800701381f
Andrew Nguyen <nguyenleminhdev@gmail.com>
2024-05-23 11:48:40 +0700

    export interface

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { WidgetChatbot } from './services/chatbot'
 
+export * from './interface'
+
 /**quản lý tương tác giữa widget và nền tảng Chat - Bot Bán Hàng*/
 export class Widget extends WidgetChatbot {}
 
